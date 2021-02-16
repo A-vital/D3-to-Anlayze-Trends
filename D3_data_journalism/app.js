372243b8ce261cb13688e507fa43a619d2f76e93
apetek <vital.amanda8@gmail.com>
2021-02-15 22:47:41 -0500

    update

---
+++ D3_data_journalism/app.js
@@ -0,0 +1,11 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
