5a643361f77663228285a550c085d58936cdc326
apetek <vital.amanda8@gmail.com>
2021-02-16 22:08:08 -0500

    update

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -42,28 +42,112 @@ var bottomAxis = d3.axisBottom(xLinearScale);
 var leftAxis = d3.axisLeft(yLinearScale);
 
 // Scale the domain
-var xMin = d3.min(healthData, function(data) {
+    var xMin;
+    var xMax;
+    var yMin;
+    var yMax;
+
+xMin = d3.min(healthData, function(data) {
     return +data.poverty * 0.95;
 });
 
-var xMax = d3.max(healthData, function(data) {
+xMax = d3.max(healthData, function(data) {
     return +data.poverty * 1.05;
 });
 
-var yMin = d3.min(healthData, function(data) {
+yMin = d3.min(healthData, function(data) {
     return +data.phys_act * 0.98;
 });
 
-var yMax = d3.max(healthData, function(data) {
+yMax = d3.max(healthData, function(data) {
     return +data.phys_act *1.02;
 });
 
 xLinearScale.domain([xMin, xMax]);
 yLinearScale.domain([yMin, yMax]);
 
- // Step 1: Append tooltip div
- var toolTip = d3.select("body")
- .append("div")
- .classed("tooltip", true);
+// Initialize tooltip 
+var toolTip = d3
+.tip()
+.attr("class", "tooltip")
+.offset([80, -60])
+.html(function(data) {
+    var stateName = data.state;
+    var pov = +data.poverty;
+    var physAct = +data.phys_act;
+    return (
+        stateName + '<br> Poverty: ' + pov + '% <br> Physically Active: ' + physAct +'%'
+    );
+});
 
-}
+// Create tooltip
+chart.call(toolTip);
+
+chart.selectAll("circle")
+.data(healthData)
+.enter()
+.append("circle")
+.attr("cx", function(data, index) {
+    return xLinearScale(data.poverty)
+})
+.attr("cy", function(data, index) {
+    return yLinearScale(data.phys_act)
+})
+.attr("r", "15")
+.attr("fill", "lightblue")
+// display tooltip on click
+.on("mouseenter", function(data) {
+    toolTip.show(data);
+})
+// hide tooltip on mouseout
+.on("mouseout", function(data, index) {
+    toolTip.hide(data);
+});
+
+// Appending a label to each data point
+chart.append("text")
+.style("text-anchor", "middle")
+.style("font-size", "12px")
+.selectAll("tspan")
+.data(healthData)
+.enter()
+.append("tspan")
+    .attr("x", function(data) {
+        return xLinearScale(data.poverty - 0);
+    })
+    .attr("y", function(data) {
+        return yLinearScale(data.phys_act - 0.2);
+    })
+    .text(function(data) {
+        return data.abbr
+    });
+
+// Append an SVG group for the xaxis, then display x-axis 
+chart
+.append("g")
+.attr('transform', `translate(0, ${height})`)
+.call(bottomAxis);
+
+// Append a group for y-axis, then display it
+chart.append("g").call(leftAxis);
+
+// Append y-axis label
+chart
+.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0-margin.left + 40)
+.attr("x", 0 - height/2)
+.attr("dy","1em")
+.attr("class", "axis-text")
+.text("Physically Active (%)")
+
+// Append x-axis labels
+chart
+.append("text")
+.attr(
+    "transform",
+    "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
+)
+.attr("class", "axis-text")
+.text("In Poverty (%)");
+};
